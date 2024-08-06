d92ae2c76cf8b69019de1e1289b30f016f2b52b3
winetree94 <winetree94@gmail.com>
2024-08-06 20:04:31 +0900

    update

---
+++ packages/06-pointer/main.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <string.h>
+
+int basic()
+{
+  printf("basic----------------\n");
+  int num = 3;
+
+  // pointer 변수 선언
+  int *pnum;
+
+  // pnum pointer 에 num 의 포인터 할당
+  pnum = &num;
+
+  printf("num 의 값: %d \n", num);
+  printf("num 의 메모리 주소값: %p \n", &num);
+  printf("pnum 의 메모리 주소값: %p \n", pnum);
+  printf("pnum 의 참조 값: %d \n", *pnum);
+
+  // pointer 의 역참조 수정, 실제 num 을 수정한 것
+  *pnum = 5;
+  printf("num 의 값: %d \n", num);
+  printf("pnum 의 참조 값: %d \n", *pnum);
+  printf("----------------\n");
+}
+
+int basic2()
+{
+  printf("basic2----------------\n");
+
+  int a = 10;
+  int b = 20;
+
+  int *ptr = &a;
+  printf("ptr 이 가리키는 변수의 값: %d \n", *ptr);
+
+  ptr = &b;
+  printf("ptr 이 가리키는 변수의 값: %d \n", *ptr);
+
+  // null pointer, 가리키는 대상이 없음
+  ptr = NULL;
+  // null pointer 를 역참조하면 오류
+  // int c = *ptr;
+
+  printf("----------------\n");
+}
+
+int double_pointer()
+{
+  printf("double pointer----------------\n");
+
+  int a = 10;
+  int *ptr_a = &a;
+
+  // 포인터의 포인터를 만들 수 있음
+  int **ptr_ptr_a = &ptr_a;
+
+  printf("1. a = %d \n", a);
+  // &와 ptr_a 는 같음 (같은 주소값)
+  printf("2. &a = %p \n", &a);
+  printf("3. ptr_a = %p \n", ptr_a);
+  // &ptr_a 와 ptr_ptr_a 는 같음 (같은 주소값)
+  printf("4. &ptr_a = %p \n", &ptr_a);
+  printf("5. ptr_ptr_a %p \n", ptr_ptr_a);
+  // &ptr_ptr_a 는 별도의 주소값
+  printf("6. &ptr_ptr_a %p \n", &ptr_ptr_a);
+
+  // ptr_ptr_a 를 역참조하면, *ptr_a 가 나옴
+  printf("7. *ptr_ptr_a %p \n", *ptr_ptr_a);
+  // ptr_ptr_a 를 2회 역참조하면 a의 값이 나옴
+  printf("8. **ptr_ptr_a %d \n", **ptr_ptr_a);
+
+  printf("----------------\n");
+}
+
+int array_and_pointer()
+{
+  printf("array and pointer----------------\n");
+
+  int a = 10;
+
+  printf("&a = %p \n", &a);
+  // 포인터에 +- 1을 하면 데이터 사이즈(int 의 경우 4byte)만큼 추가된다.
+  printf("&a - 1 = %p \n", &a - 1);
+  printf("&a + 1 = %p \n", &a + 1);
+
+  // 포인터의 연산 특성을 활용하면 배열 요소의 포인터 주소를 알 수 있다.
+  int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+  // 배열의 이름은 첫번째 요소의 포인터를 가리킨다.
+  printf("arr 의 주소: %p \n", arr);
+  for (int i = 0; i < 10; i++)
+  {
+    printf("arr[%d]의 주소: %p \n", i, &arr[i]);
+    // 배열의 이름 특성을 활용해서 아래와 같이 작성할 수도 있다.
+    // printf("arr[%d]의 주소: %p \n", i, arr + i);
+    printf("arr[%d]의 값: %d \n", i, arr[i]);
+    // 배열의 특성을 활용해서 포인터 역참조로 값에 접근할 수 있다.
+    // printf("arr[%d]의 값: %d \n", i, *(arr + i));
+  }
+
+  // 배열의 이름이 첫번째 요소를 가리키는 포인터인 덕분에
+  // 포인터를 통해서도 배열에 접근할 수 있다.
+  int *ptr = arr;
+  for (int i = 0; i < 10; i++)
+  {
+    // printf("ptr[%d]의 주소: %p \n", i, ptr + i);
+    printf("ptr[%d]의 주소: %p \n", i, &ptr[i]);
+    // printf("ptr[%d]의 값: %d", i, ptr[i]);
+    printf("ptr[%d]의 값: %d \n", i, *(ptr + i));
+  }
+
+  // pointer 를 통해서도 루프를 돌릴 수 있음
+  for (int *ptr = arr; ptr < arr + 10; ptr++)
+  {
+    printf("arr[%ld]의 값: %d \n", arr - ptr, *ptr);
+  }
+
+  printf("----------------\n");
+}
+
+int array_pointer() {
+  printf("array pointer----------------\n");
+  int arr[10];
+
+  // 모두 동일한 첫번째 요소의 포인터가 반환됨
+  printf("&arr: %p \n", &arr);
+  printf("arr: %p \n", arr);
+  printf("&arr[0]: %p \n", &arr[0]);
+
+  // 배열 포인터의 선언
+  int (*p_arr)[];
+  p_arr = &arr;
+
+  // 배열 포인터에 배열을 넣을 순 없다.
+  // p_arr = arr;
+
+  // 이제 진짜 배열의 포인터가 반환됨
+  printf("&p_arr: %p \n", &p_arr);
+
+  // 배열 포인터의 역참조를 통해 요소에 접근할 수 있음
+  printf("(*p_arr)[0]: %d \n", (*p_arr)[0]);
+
+  printf("----------------\n");
+}
+
+int main()
+{
+  basic();
+  basic2();
+  double_pointer();
+  array_and_pointer();
+  array_pointer();
+  return 0;
+}
